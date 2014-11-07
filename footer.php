9b721911bc3b48dc829e90216d911daa746a4fde
jainnidhi <jainnidhi_3@ymail.com>
2014-11-07 10:19:24 +0530

    removed default icons for contact details

--- footer.php
+++ footer.php
@@ -19,12 +19,12 @@
 	<div class="container">
 		<div class="row">
                     <div class="contact-details">
-                        <ul><?php if ( get_theme_mod('contact_email') !='' ) {  ?><li id="email"><?php echo esc_html(get_theme_mod('contact_email')); ?></li>
+                        <ul><?php if ( get_theme_mod('contact_email') !='' ) {  ?><li id="email"><?php echo get_theme_mod('contact_email'); ?></li>
 
                           <?php } else {  ?> <li id="email"> <?php esc_html_e('hello@ideaboxcreations.com', 'ethic') ?></li>
                                    <?php } ?>
 
-                          <?php if ( get_theme_mod('contact_phone') !='' ) {  ?><li id="phone"><?php echo esc_html(get_theme_mod('contact_phone')); ?></li>
+                          <?php if ( get_theme_mod('contact_phone') !='' ) {  ?><li id="phone"><?php echo get_theme_mod('contact_phone'); ?></li>
 
                           <?php } else {  ?> <li id="phone"><?php esc_html_e('0294-678456', 'ethic') ?></li>
                                    <?php } ?>
